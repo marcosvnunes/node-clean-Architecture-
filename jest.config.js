9da6cf9f3b69cb1094bca2c98f8c6b377d2c3e31
Marcos Vinicius Nunes <marcosnunescomercial@gmail.com>
2020-12-09 18:10:57 -0300

    refactor: remove file index.ts in protocols

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,10 @@
 module.exports = {
   roots: ['<rootDir>/src'],
-  collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
+  collectCoverageFrom: [
+    '<rootDir>/src/**/*.ts',
+    '!<rootDir>/src/**/*-protocols.ts',
+    '!<rootDir>/src/main/**'
+  ],
   coverageDirectory: 'coverage',
   preset: '@shelf/jest-mongodb',
   transform: {
