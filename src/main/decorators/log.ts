2f1dcebd7ae0b972d1c608ab6b23589a9052ab94
Marcos Vinicius Nunes <marcosnunescomercial@gmail.com>
2020-12-11 11:59:21 -0300

    feat: ensure LogErrorControllerDecorator calls controller with httpRequest

---
+++ src/main/decorators/log.ts
@@ -0,0 +1,13 @@
+import { Controller, HttpRequest, HttpResponse } from '../../presentation/protocols'
+
+export class LogErrorControllerDecorator implements Controller {
+  private readonly controller: Controller
+  constructor (controller: Controller) {
+    this.controller = controller
+  }
+
+  async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
+    const httpResponse = await this.controller.handle(httpRequest)
+    return httpResponse
+  }
+}
