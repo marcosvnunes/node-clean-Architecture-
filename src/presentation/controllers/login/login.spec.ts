9cc061fccf693859ee17ef0c91c1669590ef9299
Marcos Nunes <marcosnunescomercial@gmail.com>
2020-12-31 16:15:39 -0300

    refactor: remove duplicated code

--- src/presentation/controllers/login/login.spec.ts
+++ src/presentation/controllers/login/login.spec.ts
@@ -21,6 +21,12 @@ const makeFakeHttpRequest = (): HttpRequest => {
   }
 }
 
+const makeFakeHttpRequestMissingProp = (prop: string): HttpRequest => {
+  const httpRequest = makeFakeHttpRequest()
+  delete httpRequest.body[prop]
+  return httpRequest
+}
+
 const makeSut = (): any => {
   const emailValidatorStub = makeEmailValidator()
   const sut = new LoginController(emailValidatorStub)
@@ -33,21 +39,13 @@ const makeSut = (): any => {
 describe('Login Controller', () => {
   test('should return 400 if no email is provided', async () => {
     const { sut } = makeSut()
-    const httpRequest = {
-      body: {
-        password: 'any_password'
-      }
-    }
+    const httpRequest = makeFakeHttpRequestMissingProp('email')
     const httpResponse = await sut.handle(httpRequest)
     expect(httpResponse).toEqual(badRequest(new MissingParamError('email')))
   })
   test('should return 400 if no password is provided', async () => {
     const { sut } = makeSut()
-    const httpRequest = {
-      body: {
-        email: 'any_email@mail.com'
-      }
-    }
+    const httpRequest = makeFakeHttpRequestMissingProp('password')
     const httpResponse = await sut.handle(httpRequest)
     expect(httpResponse).toEqual(badRequest(new MissingParamError('password')))
   })
