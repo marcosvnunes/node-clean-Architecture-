3e7cf457bf05d727e9ee9f6b621fa92aa07df11a
Marcos Nunes <marcosnunescomercial@gmail.com>
2021-01-01 19:10:36 -0300

    test: ensure Validation Composite return an error if validations fails

---
+++ src/presentation/helpers/validators/validation-composite.spec.ts
@@ -0,0 +1,18 @@
+import { InvalidParamError } from '../../erros'
+import { Validation } from './validation'
+import { ValidationComposite } from './validation-composite'
+
+describe('Validation Composite', () => {
+  test('should return an Error if validations fails', () => {
+    class ValidationStub implements Validation {
+      validate (input: any): Error {
+        return null
+      }
+    }
+    const validationStub = new ValidationStub()
+    const sut = new ValidationComposite([validationStub])
+    jest.spyOn(validationStub, 'validate').mockReturnValueOnce(new InvalidParamError('field'))
+    const error = sut.validate({ any_field: 'any_value' })
+    expect(error).toEqual(new InvalidParamError('field'))
+  })
+})
