c59a5f2bab9458d057098f4259b1d65526c652bb
Marcos Nunes <marcosnunescomercial@gmail.com>
2021-01-01 19:20:47 -0300

    refactor: move sut creation to a factory method

--- src/presentation/helpers/validators/validation-composite.spec.ts
+++ src/presentation/helpers/validators/validation-composite.spec.ts
@@ -2,42 +2,45 @@ import { InvalidParamError } from '../../erros'
 import { Validation } from './validation'
 import { ValidationComposite } from './validation-composite'
 
+interface SutTypes {
+  sut: ValidationComposite
+  validationStub: Validation
+  validationStub2: Validation
+}
+
+const makeSut = (): SutTypes => {
+  class ValidationStub implements Validation {
+    validate (input: any): Error {
+      return null
+    }
+  }
+  const validationStub = new ValidationStub()
+  const validationStub2 = new ValidationStub()
+  const sut = new ValidationComposite([validationStub, validationStub2])
+  return {
+    sut,
+    validationStub,
+    validationStub2
+  }
+}
+
 describe('Validation Composite', () => {
   test('should return an Error if validations fails', () => {
-    class ValidationStub implements Validation {
-      validate (input: any): Error {
-        return null
-      }
-    }
-    const validationStub = new ValidationStub()
-    const sut = new ValidationComposite([validationStub])
+    const { sut, validationStub } = makeSut()
     jest.spyOn(validationStub, 'validate').mockReturnValueOnce(new InvalidParamError('field'))
     const error = sut.validate({ any_field: 'any_value' })
     expect(error).toEqual(new InvalidParamError('field'))
   })
 
   test('should return an Error if any validations fails', () => {
-    class ValidationStub implements Validation {
-      validate (input: any): Error {
-        return null
-      }
-    }
-    const validationStub = new ValidationStub()
-    const validationStub2 = new ValidationStub()
+    const { sut, validationStub2 } = makeSut()
     jest.spyOn(validationStub2, 'validate').mockReturnValueOnce(new InvalidParamError('field'))
-    const sut = new ValidationComposite([validationStub, validationStub2])
     const error = sut.validate({ any_field: 'any_value' })
     expect(error).toEqual(new InvalidParamError('field'))
   })
 
   test('should no return on success', () => {
-    class ValidationStub implements Validation {
-      validate (input: any): Error {
-        return null
-      }
-    }
-    const validationStub = new ValidationStub()
-    const sut = new ValidationComposite([validationStub])
+    const { sut } = makeSut()
     const error = sut.validate({ any_field: 'any_value' })
     expect(error).toBeFalsy()
   })
