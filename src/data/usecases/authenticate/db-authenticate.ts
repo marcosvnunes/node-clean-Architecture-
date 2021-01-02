702fcba02815ef5cd51393914512713979c15f93
Marcos Nunes <marcosnunescomercial@gmail.com>
2021-01-02 15:46:53 -0300

    feat: ensure DbAutheticate calls loadAccountByEmailRepository with correct email

---
+++ src/data/usecases/authenticate/db-authenticate.ts
@@ -0,0 +1,14 @@
+import { Authenticate, AuthenticateModel } from '../../../domain/usercases/authenticate'
+import { LoadAccountByEmailRepository } from '../../protocols/load-account-by-email-repository'
+
+export class DbAuthenticate implements Authenticate {
+  private readonly loadAccountByEmailRepository: LoadAccountByEmailRepository
+  constructor (loadAccountByEmailRepository: LoadAccountByEmailRepository) {
+    this.loadAccountByEmailRepository = loadAccountByEmailRepository
+  }
+
+  async auth (authenticate: AuthenticateModel): Promise<string> {
+    await this.loadAccountByEmailRepository.load(authenticate.email)
+    return null
+  }
+}
