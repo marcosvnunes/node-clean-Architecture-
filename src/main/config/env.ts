9883c69b1dde01c401bcf659d09349e20ce5b6f8
Marcos Nunes <marcosnunescomercial@gmail.com>
2021-01-05 14:56:13 -0300

    chore: engines with node vesion

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,7 +1,6 @@
 export const env = {
   port: process.env.PORT || 5050,
   mongoUrl: process.env.MONGO_URL ||
-  'mongodb+srv://tickedev:651588@cluster0.9vtgm.mongodb.net/<dbname>?retryWrites=true&w=majority',
+  '',
   secretKey: process.env.SECRET_KEY || '0000000000'
-
 }
