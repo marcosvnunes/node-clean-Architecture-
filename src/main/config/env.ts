1e713c442113b4ee4c371a6ddecd703491c5fb0c
Marcos Nunes <marcosnunescomercial@gmail.com>
2021-01-05 18:34:14 -0300

    chore: add docker-compose

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,6 +1,6 @@
 export const env = {
   port: process.env.PORT || 5050,
   mongoUrl: process.env.MONGO_URL ||
-  '',
+  'mongodb://mongo:27017/clean-node-api',
   secretKey: process.env.SECRET_KEY || '0000000000'
 }
