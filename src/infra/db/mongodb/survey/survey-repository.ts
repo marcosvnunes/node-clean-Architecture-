0857cc8bf474a9122f750aa8126b5d9904f12b08
Marcos Nunes <marcosnunescomercial@gmail.com>
2021-01-09 11:20:06 -0300

    feat: ensure SurveyMongoRepository create survey on add success

---
+++ src/infra/db/mongodb/survey/survey-repository.ts
@@ -0,0 +1,9 @@
+import { AddSurveyRepository, AddSurveyModel } from './survey-repository-protocols'
+import { MongoHelper } from '../helpers/mongo-helper'
+
+export class SurveyMongoRepository implements AddSurveyRepository {
+  async add (SurveyData: AddSurveyModel): Promise<void> {
+    const surveyCollection = await MongoHelper.getCollection('survey')
+    await surveyCollection.insertOne(SurveyData)
+  }
+}
