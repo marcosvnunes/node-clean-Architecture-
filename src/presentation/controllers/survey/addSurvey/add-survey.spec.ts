a3b17803347d401e80f8deac990ff68ae4f9fde8
Marcos Nunes <marcosnunescomercial@gmail.com>
2021-01-08 23:21:00 -0300

    refactor: create factory to FakeRequest

--- src/presentation/controllers/survey/addSurvey/add-survey.spec.ts
+++ src/presentation/controllers/survey/addSurvey/add-survey.spec.ts
@@ -1,6 +1,16 @@
 import { SurveyController } from './add-survey'
 import { Validation } from '../../../protocols/validation'
 
+const makeFakeRequest = ({
+  body: {
+    question: 'any_question',
+    answers: [{
+      answer: 'any_answer',
+      image: 'any_image'
+    }]
+  }
+})
+
 describe('AddSurvey Controller', () => {
   test('should call validation with correct values', async () => {
     class ValidationStub implements Validation {
@@ -12,16 +22,7 @@ describe('AddSurvey Controller', () => {
     const validationStub = new ValidationStub()
     const sut = new SurveyController(validationStub)
     const validateSpy = jest.spyOn(validationStub, 'validate')
-    const httpRequest = {
-      body: {
-        question: 'any_question',
-        answers: [{
-          answer: 'any_answer',
-          image: 'any_image'
-        }]
-      }
-    }
-    await sut.handle(httpRequest)
-    expect(validateSpy).toHaveBeenCalledWith(httpRequest.body)
+    await sut.handle(makeFakeRequest)
+    expect(validateSpy).toHaveBeenCalledWith(makeFakeRequest.body)
   })
 })
