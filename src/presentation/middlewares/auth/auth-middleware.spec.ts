66d3007461a28c2ac2526d35e33251300817ff21
Marcos Nunes <marcosnunescomercial@gmail.com>
2021-01-10 13:58:22 -0300

    test: ensure AuthMiddleware returns 403 if loadAccountByToken fails

--- src/presentation/middlewares/auth/auth-middleware.spec.ts
+++ src/presentation/middlewares/auth/auth-middleware.spec.ts
@@ -49,4 +49,12 @@ describe('Auth middleware', () => {
     await sut.handle(makeFakeRequest())
     expect(loadSpy).toHaveBeenCalledWith('any_token')
   })
+
+  test('should return 403 if loadAccountByToken fails', async () => {
+    const { sut, loadAccountByTokenStub } = makeSut()
+    jest.spyOn(loadAccountByTokenStub, 'load')
+      .mockReturnValueOnce(new Promise(resolve => resolve(null)))
+    const httpResponse = await sut.handle(makeFakeRequest())
+    expect(httpResponse).toEqual(forbidden(new AccessDeniedError()))
+  })
 })
