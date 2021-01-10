0f30f43e187e1bac41e770f21aef8d342bba7466
Marcos Nunes <marcosnunescomercial@gmail.com>
2021-01-09 22:22:09 -0300

    test: ensure SurveyRoute POST return 204 on success

---
+++ src/main/routes/survey-routes.test.ts
@@ -0,0 +1,34 @@
+import { Collection } from 'mongodb'
+import request from 'supertest'
+import { MongoHelper } from '../../infra/db/mongodb/helpers/mongo-helper'
+import app from '../config/app'
+
+let accountCollection: Collection
+describe('Login Routes', () => {
+  beforeAll(async () => {
+    await MongoHelper.connect(process.env.MONGO_URL)
+  })
+
+  beforeEach(async () => {
+    accountCollection = await MongoHelper.getCollection('accounts')
+    await accountCollection.deleteMany({})
+  })
+
+  afterAll(async () => {
+    await MongoHelper.disconnect()
+  })
+  describe('Survey Post ', () => {
+    test('should return 204 on success', async () => {
+      await request(app)
+        .post('/api/survey')
+        .send({
+          question: 'question',
+          answers: [{
+            answer: 'answer',
+            image: 'http://image.com.br'
+          }]
+        })
+        .expect(204)
+    })
+  })
+})
