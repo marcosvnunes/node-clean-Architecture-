4917020da3a319e324ed061ddafbddd78c28548d
Marcos Nunes <marcosnunescomercial@gmail.com>
2021-01-13 19:25:56 -0300

    feat: ensure Survey Post return 204 if accessToken valid is provided

--- src/infra/db/mongodb/survey/survey-repository.ts
+++ src/infra/db/mongodb/survey/survey-repository.ts
@@ -3,7 +3,7 @@ import { MongoHelper } from '../helpers/mongo-helper'
 
 export class SurveyMongoRepository implements AddSurveyRepository {
   async add (SurveyData: AddSurveyModel): Promise<void> {
-    const surveyCollection = await MongoHelper.getCollection('survey')
+    const surveyCollection = await MongoHelper.getCollection('surveys')
     await surveyCollection.insertOne(SurveyData)
   }
 }
