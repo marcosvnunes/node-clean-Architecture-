916c667ef8e5da6dfca3bf01c72311c746ac2526
Marcos Nunes <marcosnunescomercial@gmail.com>
2021-01-17 00:59:31 -0300

    feat: ensure DbLoadSurveys calls LoadSurveysRepository

---
+++ src/data/usecases/load-surveys/db-load-surveys.ts
@@ -0,0 +1,11 @@
+import { SurveyModel } from '../../../domain/models/survey'
+import { LoadSurvey } from '../../../domain/usercases/load-surveys'
+import { LoadSurveysRepository } from '../../protocols/db/survey/load-surveys-repository'
+
+export class DbLoadSurveys implements LoadSurvey {
+  constructor (private readonly loadSurveysRepository: LoadSurveysRepository) {}
+  async load (): Promise<SurveyModel[]> {
+    await this.loadSurveysRepository.loadAll()
+    return []
+  }
+}
