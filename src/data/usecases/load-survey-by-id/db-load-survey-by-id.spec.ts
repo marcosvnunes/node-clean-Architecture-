6de1fd5fdaf56b72c02a74206e9d2856cb65f099
Marcos Nunes <marcosnunescomercial@gmail.com>
2021-01-20 12:36:54 -0300

    refactor: move dbLoadSurveyById protocols to a single file

--- src/data/usecases/load-survey-by-id/db-load-survey-by-id.spec.ts
+++ src/data/usecases/load-survey-by-id/db-load-survey-by-id.spec.ts
@@ -1,5 +1,4 @@
-import { SurveyModel } from '../../../domain/models/survey'
-import { LoadSurveyByIdRepository } from '../../protocols/db/survey/load-survey-by-id-repository'
+import { SurveyModel, LoadSurveyByIdRepository } from './db-load-survey-by-id-protocols'
 import { DbLoadSurveyById } from './db-load-survey-by-id'
 
 const makeFakeSurveys = (): SurveyModel => ({
