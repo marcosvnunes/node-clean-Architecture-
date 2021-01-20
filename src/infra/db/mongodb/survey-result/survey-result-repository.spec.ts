28dfae2eb6be34024c573d98300785b297b0d3e7
Marcos Nunes <marcosnunescomercial@gmail.com>
2021-01-20 19:36:59 -0300

    test: ensure SuveyResultRepository insert a new surveyResult if its new

---
+++ src/infra/db/mongodb/survey-result/survey-result-repository.spec.ts
@@ -0,0 +1,70 @@
+import { Collection } from 'mongodb'
+import { AccountModel } from '../../../../domain/models/account'
+import { SurveyModel } from '../../../../domain/models/survey'
+import { MongoHelper } from '../helpers/mongo-helper'
+import { SurveyResultMongoRepository } from './survey-result-repository'
+
+let surveyCollection: Collection
+let surveyResultCollection: Collection
+let accountCollection: Collection
+
+const makeSut = (): SurveyResultMongoRepository => {
+  return new SurveyResultMongoRepository()
+}
+
+const makeSurvey = async (): Promise<SurveyModel> => {
+  const res = await surveyCollection.insertOne({
+    question: 'any_question',
+    answers: [{
+      answer: 'any_answer',
+      image: 'any_image'
+    }],
+    date: new Date()
+  })
+  return MongoHelper.map(res.ops[0])
+}
+
+const makeAccount = async (): Promise<AccountModel> => {
+  const res = await surveyCollection.insertOne({
+    name: 'any_name',
+    email: 'any_email@mail.com',
+    password: 'any_password'
+  })
+  return MongoHelper.map(res.ops[0])
+}
+
+describe('SurveyResult Mongo Repository', () => {
+  beforeAll(async () => {
+    await MongoHelper.connect(process.env.MONGO_URL)
+  })
+
+  beforeEach(async () => {
+    surveyCollection = await MongoHelper.getCollection('surveys')
+    await surveyCollection.deleteMany({})
+    surveyResultCollection = await MongoHelper.getCollection('surveyResults')
+    await surveyResultCollection.deleteMany({})
+    accountCollection = await MongoHelper.getCollection('accounts')
+    await accountCollection.deleteMany({})
+  })
+
+  afterAll(async () => {
+    await MongoHelper.disconnect()
+  })
+
+  describe('save()', () => {
+    test('should insert a new surveyResult if its new ', async () => {
+      const account = await makeAccount()
+      const survey = await makeSurvey()
+      const sut = makeSut()
+      const surveyResult = await sut.save({
+        accountId: account.id,
+        surveyId: survey.id,
+        answer: survey.answers[0].answer,
+        date: new Date()
+      })
+      expect(surveyResult).toBeTruthy()
+      expect(surveyResult.id).toBeTruthy()
+      expect(surveyResult.answer).toBe(survey.answers[0].answer)
+    })
+  })
+})
