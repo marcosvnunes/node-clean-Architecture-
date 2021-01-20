dd1ec11a15c235ceccaaaf90ffae79e11d25366a
Marcos Nunes <marcosnunescomercial@gmail.com>
2021-01-20 19:36:29 -0300

    feat: ensure SuveyResultRepository insert a new surveyResult if its new

---
+++ src/infra/db/mongodb/survey-result/survey-result-repository.ts
@@ -0,0 +1,23 @@
+import { MongoHelper } from '../helpers/mongo-helper'
+import { SaveSurveyResultRepository } from '../../../../data/protocols/db/survey/save-survey-result-repository'
+import { SaveSurveyResultModel } from '../../../../domain/usercases/save-survey-result'
+import { SurveyResultModel } from '../../../../domain/models/survey-result'
+
+export class SurveyResultMongoRepository implements SaveSurveyResultRepository {
+  async save (data: SaveSurveyResultModel): Promise<SurveyResultModel> {
+    const surveyResultCollection = await MongoHelper.getCollection('surveyResults')
+    const res = await surveyResultCollection.findOneAndUpdate({
+      accountId: data.accountId,
+      surveyId: data.surveyId
+    }, {
+      $set: {
+        answer: data.answer,
+        date: data.date
+      }
+    }, {
+      upsert: true,
+      returnOriginal: false
+    })
+    return res.value && MongoHelper.map(res.value)
+  }
+}
