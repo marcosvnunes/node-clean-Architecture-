75783c9fbd41c15ef9e628dd7fac5a8d9f5b6083
Marcos Nunes <marcosnunescomercial@gmail.com>
2021-01-20 22:48:01 -0300

    feat: ensure SurveyResultController returns 403 if loadSurveyById fails

--- src/presentation/controllers/survey-result/save-survey-result/save-survey-result.ts
+++ src/presentation/controllers/survey-result/save-survey-result/save-survey-result.ts
@@ -1,11 +1,16 @@
 import { LoadSurveyById } from '../../../../domain/usercases/load-survey-by-id'
+import { InvalidParamError } from '../../../erros'
+import { forbidden } from '../../../helpers/http/http-helper'
 import { Controller, HttpRequest, HttpResponse } from './save-survey-result-protocols'
 
 export class SurveyResultController implements Controller {
   constructor (private readonly loadSurveyById: LoadSurveyById) {}
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
     const surveyId = httpRequest.params.surveyId
-    await this.loadSurveyById.loadById(surveyId)
+    const survey = await this.loadSurveyById.loadById(surveyId)
+    if (!survey) {
+      return forbidden(new InvalidParamError('surveyId'))
+    }
     return null
   }
 }
