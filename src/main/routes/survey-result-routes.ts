5cc4816c3fce8246dd76838e9073a73787a3029d
Marcos Nunes <marcosnunescomercial@gmail.com>
2021-01-26 13:34:45 -0300

    feat: ensure SurveyResult route put returns 403 if accessToken no is provided

---
+++ src/main/routes/survey-result-routes.ts
@@ -0,0 +1,8 @@
+import { Router } from 'express'
+import { adaptRoute } from '../adapters/express/express-routes-adapter'
+import { makeSaveSurveyResultController } from '../factories/controllers/survey-result/save-survey-result-factory'
+import { auth } from '../middlewares/auth'
+
+export default (router: Router): void => {
+  router.put('/survey/:survey_id/results', auth, adaptRoute(makeSaveSurveyResultController()))
+}
